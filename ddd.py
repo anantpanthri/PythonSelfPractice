6305143a31fa0743c81950ab6912e847863357ba
ANANT PANTHRI <anant.shripanthri@gmail.com>
2017-05-05 19:58:33 -0500

    Add files via upload

---
+++ ddd.py
@@ -0,0 +1,14 @@
+import urllib 
+def read_test():
+    file_read = open(r"C:\mooo\test.txt")
+    text=file_read.read()
+    file_read.close()
+    check_profanity(text)
+
+def check_profanity(text):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text)
+    output=connection.read()
+    print(output)
+    connection.close()
+
+read_test()
